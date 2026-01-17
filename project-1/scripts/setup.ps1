f57a4592c9301be2ea127afd79875cd4e8ae2d06
Daniel Zhang <142930638+zhangdaniel62@users.noreply.github.com>
2026-01-17 00:48:24 -0600

    Add setup script for VM configuration
    This script configures the VM by installing essential software.

---
+++ project-1/scripts/setup.ps1
@@ -0,0 +1,11 @@
+# Requires Powershell 2.0 or higher
+
+<#
+This file contains a script that will be run on setup to configure the VM to do the following:
+
+  1. Install a code editor: Visual Studio Code
+  2. Install a compiler: G++
+  3. Install an IDE: Visual Studio
+  4. Install a document editor: Libreoffice
+  5. Install a PDF viewer: Adobe Acrobat PDF
+#>
