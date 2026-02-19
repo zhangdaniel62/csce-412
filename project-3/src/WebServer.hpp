fde43be1c88624a7ccce7234ead7b45327468397
Daniel Zhang <zhangdaniel62@gmail.com>
2026-02-19 03:29:57 -0600

    added header file for Webserver

--- project-3/src/WebServer.hpp
+++ project-3/src/WebServer.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "Request.hpp"
+
+class WebServer {
+    public:
+        WebServer(int id);
+        void assign(Request req);
+        void processCycle();
+        bool isBusy();
+    private:
+        int id;
+        Request req;
+        bool busy;
+
+};
