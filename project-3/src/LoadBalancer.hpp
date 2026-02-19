ed2d8c049b726a10c3a0f1a36c05df78e33fc8f7
Daniel Zhang <zhangdaniel62@gmail.com>
2026-02-19 03:41:44 -0600

    added framework for LoadBalancer.hpp

--- project-3/src/LoadBalancer.hpp
+++ project-3/src/LoadBalancer.hpp
@@ -1,1 +1,34 @@
 #pragma once
+
+#include <queue>
+#include <vector>
+
+#include "Request.hpp"
+#include "WebServer.hpp"
+#include "IPBlocker.hpp"
+
+using std::queue;
+using std::vector;
+
+class LoadBalancer {
+    public:
+        LoadBalancer(int numServers, int cooldown);
+        void run(int totalCycles);
+        void addRequest(Request req);
+    private:
+        queue<Request> requestQueue;
+        vector<WebServer> servers;
+        int clock;
+        int cooldown;
+        int lastCheckedClock;
+        IPBlocker blocker;
+
+        void simulateCycle();
+        void generateRequest();
+        void assignRequests();
+        void checkScale();
+        bool underMinthreshold();
+        bool aboveMaxthreshold();
+        void addServer();
+        void removeServer();
+};
