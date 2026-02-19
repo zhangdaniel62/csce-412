62e33281f0dc5114e40a48c89ffb3e37c9181c71
Daniel Zhang <zhangdaniel62@gmail.com>
2026-02-19 03:36:04 -0600

    added implementation for WebServer.cpp

---
+++ project-3/src/webserver.cpp
@@ -0,0 +1,22 @@
+#include "WebServer.hpp"
+
+WebServer::WebServer(int id) : id(id), busy(false), req() {}
+
+void WebServer::assign(Request req) {
+    this->req = req;
+    busy = true;
+}
+
+void WebServer::processCycle() {
+    if (busy) {
+        req.decrementTime(1);
+
+        if (req.isComplete()) {
+            busy = false;
+        }
+    }
+}
+
+bool WebServer::isBusy() {
+    return busy;
+}
